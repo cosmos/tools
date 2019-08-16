0e9405f038e3465aea4c679b16b9135140598c8f
mircea-c <mircea@tendermint.com>
2019-08-16 12:02:27 -0400

    aws library used by runsim

---
+++ lib/runsimaws/runsimaws.go
@@ -0,0 +1,89 @@
+package runsimaws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	ddb "github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+// DynamoDB utilities used by the runsim application
+//
+
+type DdbTable struct {
+	svc        *ddb.DynamoDB
+	PrimaryKey *string
+	Name       *string
+}
+
+func (table *DdbTable) Config(awsRegion, primaryKey, tableName string) {
+	table.PrimaryKey = &primaryKey
+	table.Name = &tableName
+	table.svc = ddb.New(session.Must(session.NewSession(&aws.Config{Region: aws.String(awsRegion)})))
+}
+
+func (table *DdbTable) GetState(key string, data interface{}) (err error) {
+	queryResult, err := table.svc.GetItem(&ddb.GetItemInput{
+		Key:       map[string]*ddb.AttributeValue{*table.PrimaryKey: {S: aws.String(key)}},
+		TableName: aws.String(*table.Name),
+	})
+	if err != nil {
+		return
+	}
+	err = dynamodbattribute.UnmarshalMap(queryResult.Item, &data)
+	return
+}
+
+func (table *DdbTable) PutState(data interface{}) (err error) {
+	attributes, err := dynamodbattribute.MarshalMap(data)
+	if err != nil {
+		return
+	}
+	_, err = table.svc.PutItem(&ddb.PutItemInput{
+		TableName: table.Name,
+		Item:      attributes,
+	})
+	return
+}
+
+func (table *DdbTable) UpdateState(key string, data interface{}) (err error) {
+	attributes, err := dynamodbattribute.MarshalMap(data)
+	if err != nil {
+		return
+	}
+	_, err = table.svc.UpdateItem(&ddb.UpdateItemInput{
+		Key:       map[string]*ddb.AttributeValue{*table.PrimaryKey: {S: aws.String(key)}},
+		TableName: table.Name,
+		ExpressionAttributeNames: map[string]*string{
+			"#s": aws.String("Status"),
+			"#n": aws.String("NumInstances"),
+		},
+		UpdateExpression:          aws.String("set #s = :s, #n = :n"),
+		ExpressionAttributeValues: attributes,
+	})
+	return
+}
+
+// SSM functions used by the runsim application
+//
+
+type Ssm struct {
+	svc *ssm.SSM
+}
+
+func (params *Ssm) Config(awsRegion string) {
+	params.svc = ssm.New(session.Must(session.NewSession(&aws.Config{Region: aws.String(awsRegion)})))
+}
+
+func (params *Ssm) GetParameter(parameterName string) (paramValue string, err error) {
+	getParamOutput, err := params.svc.GetParameter(&ssm.GetParameterInput{
+		Name:           &parameterName,
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		return
+	}
+	paramValue = *getParamOutput.Parameter.Value
+	return
+}
